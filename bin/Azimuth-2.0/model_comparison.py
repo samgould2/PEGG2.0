fc05786f180b6556c7271d618a65c56a631084d7
Sam Gould <80423022+samgould2@users.noreply.github.com>
2023-09-28 13:10:08 -0400

    more cleaning

--- bin/Azimuth-2.0/model_comparison.py
+++
@@ -1,1 +0,0 @@
-azimuth/model_comparison.py
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+
+    name = 'pegg',
+    author = 'Samuel Gould',
+    author_email = 'samgould@mit.edu',
+    url = 'https://github.com/samgould2/PEGG',
+    version = '1.0.6',
+    description = 'Prime Editing Guide Generator',
+    py_modules = ["pegg"],
+    package_dir = {'': 'src'},
+
+    install_requires = ["Bio>=1.4.0",
+        "cyvcf2>=0.30.18",
+        "matplotlib>=3.5.1",
+        "mock>=4.0.3",
+        "numpy>=1.21.5",
+        "pandas>=1.4.2",
+        "seaborn>=0.11.2",
+        "setuptools>=61.2.0",
+        "Sphinx>=4.4.0"
+    ],
+
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.9",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"
+        ]
+)
